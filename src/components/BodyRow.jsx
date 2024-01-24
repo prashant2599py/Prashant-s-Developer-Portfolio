47dcda9f5626226c723682280ba8b21afc058a87
Prashant Lodhi <prashantrajput2517@gmail.com>
2024-01-24 15:55:11 +0530

    importing image and body alignment

--- src/components/BodyRow.jsx
+++ src/components/BodyRow.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import Buttons from './Buttons'
-// import './Body.css'
+import image1 from "./store/images/bg_img.png"
+
 
 function BodyRow() {
     const name = "Prashant Lodhi"
+
   return (
-    <div className=' w-screen h-screen bg-black text-white'>
-        <section>
-            <div className='text-4xl'>Hi, My name is <br /> <span className='text-4xl
-            text-pink-700'>{name}</span></div>
-            <div className='text-4xl'>And I am a passionate</div>
-            <Buttons ></Buttons>
-            
-        </section>
-    </div>
+
+    <div className="parent-div h-screen bg-black text-white ">
+      <section className='flex'>
+            <div className='w-1/5 text-4xl py-12 px-12'>Hi, my name is <br /> <span className='text-4xl
+                text-pink-700'>{name}</span> 
+    
+                <div className='text-4xl'>And I am a passionate</div>
+
+                <div className='py-12 px-0'>
+                  <Buttons />
+                </div>
+            </div>
+
+            <div className='w-4/5 mx-0 my-4' >
+                <img src={image1} alt="developer image"  style={{height: 300, width: 300, position: 'absolute' }}/>
+            </div>
+      </section>
+    </div>  
   )
 }
 
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,14 +2,11 @@ import React from 'react'
 
 function Buttons() {
 
-    function handleClick(){
-        
-    }
   return (
     <div>
         <div >
-            <button className='text-base rounded-lg  cursor-pointer px-3 py-3  border-solid border 2 border-white'>Resume</button>
-            <button className='text-base rounded-lg  cursor-pointer px-3 py-3  border-solid border 2 border-white' onClick={()=>{window.open('https://github.com/prashant2599py', '_blank')}}>Visit Github</button>
+            <button className='text-lg rounded-lg  cursor-pointer px-3 py-3  border-solid border 2 border-white'>Resume</button>
+            <button className='text-lg rounded-lg  cursor-pointer px-3 py-3  border-solid border 2 border-white' onClick={()=>{window.open('https://github.com/prashant2599py', '_blank')}}>Visit Github</button>
         </div>
     </div>
   )
